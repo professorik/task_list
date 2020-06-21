282f4668c94d0ec5d19fec944e3b7cc891f49e63
Tony <boreykotony@gmail.com>
2020-06-21 13:28:02 +0300

    add localStorage

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,5 +5,13 @@ import store from './store/store.js'
 new Vue({
   el: '#app',
   store: store,
-  render: h => h(App)
+  render: h => h(App),
+  beforeCreate() {
+		this.$store.commit('initialiseStore');
+	}
 })
+
+store.subscribe((mutation, state) => {
+	// Store the state object as a JSON string
+	localStorage.setItem('store', JSON.stringify(state));
+});
--- vue/src/store/store.js
+++ vue/src/store/store.js
@@ -32,6 +32,15 @@ export default new Vuex.Store({
         }
     },
     mutations: {
+      initialiseStore(state) {
+			// Check if the ID exists
+  			if(localStorage.getItem('store')) {
+  				// Replace the state object with the stored item
+  				this.replaceState(
+  					Object.assign(state, JSON.parse(localStorage.getItem('store')))
+  				);
+  			}
+  		},
       addTask (state, task) {
           state.todos.push({
   					id: state.todos.length + 1,
